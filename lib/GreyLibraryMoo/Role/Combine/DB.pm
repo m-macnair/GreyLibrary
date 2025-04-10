d715425155961686b6c581c29dbf2e582a22f295
m <mmacnair.org>
2025-04-10 23:23:51 +0100

    Inheritaaaaaaaaaaaaaaaaaaance

--- lib/GreyLibraryMoo/Role/Combine/DB.pm
+++ lib/GreyLibraryMoo/Role/Combine/DB.pm
@@ -1,15 +1,17 @@
 # ABSTRACT : Combine ./foreign/ DB modules into one role for consistency
 package GreyLibraryMoo::Role::Combine::DB;
-our $VERSION = 'v0.0.14';
-##~ DIGEST : c12853d8557a63e21c1b3c680487c7a1
+our $VERSION = 'v0.0.15';
+##~ DIGEST : 98914239ab4a5964d3a046bb938f4e0c
 use Moo::Role;
 with qw/
   Moo::GenericRole::DB
   Moo::GenericRole::DB::Abstract
   Moo::GenericRole::DB::SQLite
-  Moo::Task::FileDB::Role::DB::AbstractSQLite
-  Moo::Task::SubjectTagDB::Role::DB::AbstractSQLite
+
+  Moo::Task::FileDB::Role::DB
+
   Moo::Task::SubjectTagDB::Role::Core
+  Moo::Task::SubjectTagDB::Role::DB
   Moo::Task::SubjectTagDB::Role::CacheWrappers::FastMmap
   /;
 1;
--- lib/GreyLibraryMoo/Role/Combine/DB/MariaDB.pm
+++ lib/GreyLibraryMoo/Role/Combine/DB/MariaDB.pm
@@ -1,18 +1,12 @@
 # ABSTRACT : Combine ./foreign/ DB modules into one role for consistency
 package GreyLibraryMoo::Role::Combine::DB::MariaDB;
-our $VERSION = 'v0.0.3';
-##~ DIGEST : bfcf77d2d26495e37ecdf954a9492bcb
+our $VERSION = 'v0.0.4';
+##~ DIGEST : 116002c9cb73e2f12b41c45321ea2e3e
 use Moo::Role;
 
 with qw/
-  Moo::GenericRole::DB
-  Moo::GenericRole::DB::Abstract
-  Moo::GenericRole::DB::MariaMysql
-
-  Moo::Task::SubjectTagDB::Role::Core
-  Moo::Task::SubjectTagDB::Role::DB
+  GreyLibraryMoo::Role::Combine::DB
   Moo::Task::SubjectTagDB::Role::DB::MariaDB
-  Moo::Task::SubjectTagDB::Role::CacheWrappers::FastMmap
   /;
 
 sub BUILD {
